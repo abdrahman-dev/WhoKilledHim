06d1f4c9d57c999037574771a20cee3acff89b32
unknown <Ryzuki4526>
2026-05-16 18:14:24 +0300

    add 2 cases

--- client/src/cases/index.ts
+++ client/src/cases/index.ts
@@ -1,6 +1,10 @@
 import type { Case } from '../types/case.types';
 import { trainingCase } from './training-case';
+import { museumCase } from './golden-thief';
+import { drRamzyCase } from './ramzy-disapperance'
 
 export const availableCases: Case[] = [
   trainingCase,
+  museumCase,
+  drRamzyCase
 ];
